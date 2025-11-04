2101d5057ee5a31ad51eb38adc1cb9332fe81c39
jeloing-kod <lkjeloingfd>
2025-11-04 18:29:33 +0800

    Add project files.

---
+++ file_processing/Form1.vb
@@ -0,0 +1,23 @@
+﻿Imports System.IO
+
+Public Class Form1
+   ﻿Imports System.IO
+
+Public Class Form1
+        Private Sub ButtonWrite_Click(sender As Object, e As EventArgs) Handles ButtonWrite.Click
+            Try
+                Dim filePath As String = "sample.txt"
+
+                Using writer As New StreamWriter(filePath)
+                    writer.WriteLine("Hello, this is a test file!")
+                    writer.WriteLine("Second line of text.")
+                End Using
+
+                MessageBox.Show("Data written successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+
+        End Sub
+    End Class
+End Class
