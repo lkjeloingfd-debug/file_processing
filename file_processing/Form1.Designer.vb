0fe1e2ce08aa1055d8c6268aa057d06d37a19fdf
jeloing-kod <lkjeloingfd>
2025-11-04 19:36:23 +0800

    addded read

--- file_processing/Form1.Designer.vb
+++ file_processing/Form1.Designer.vb
@@ -24,6 +24,8 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.ButtonWrite = New System.Windows.Forms.Button()
         Me.ButtonRead = New System.Windows.Forms.Button()
+        Me.ButtonReadPerLine = New System.Windows.Forms.Button()
+        Me.ListBox = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
         '
         'ButtonWrite
@@ -44,11 +46,30 @@ Partial Class Form1
         Me.ButtonRead.Text = "read"
         Me.ButtonRead.UseVisualStyleBackColor = True
         '
+        'ButtonReadPerLine
+        '
+        Me.ButtonReadPerLine.Location = New System.Drawing.Point(385, 207)
+        Me.ButtonReadPerLine.Name = "ButtonReadPerLine"
+        Me.ButtonReadPerLine.Size = New System.Drawing.Size(197, 102)
+        Me.ButtonReadPerLine.TabIndex = 2
+        Me.ButtonReadPerLine.Text = "asjknd"
+        Me.ButtonReadPerLine.UseVisualStyleBackColor = True
+        '
+        'ListBox
+        '
+        Me.ListBox.FormattingEnabled = True
+        Me.ListBox.Location = New System.Drawing.Point(136, 207)
+        Me.ListBox.Name = "ListBox"
+        Me.ListBox.Size = New System.Drawing.Size(197, 108)
+        Me.ListBox.TabIndex = 3
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListBox)
+        Me.Controls.Add(Me.ButtonReadPerLine)
         Me.Controls.Add(Me.ButtonRead)
         Me.Controls.Add(Me.ButtonWrite)
         Me.Name = "Form1"
@@ -59,4 +80,6 @@ Partial Class Form1
 
     Friend WithEvents ButtonWrite As Button
     Friend WithEvents ButtonRead As Button
+    Friend WithEvents ButtonReadPerLine As Button
+    Friend WithEvents ListBox As ListBox
 End Class
--- file_processing/Form1.vb
+++ file_processing/Form1.vb
@@ -1,11 +1,10 @@
 ﻿Imports System.IO
 
 Public Class Form1
+    Dim filepath As String = "sample.txt"
     Private Sub ButtonWrite_Click(sender As Object, e As EventArgs) Handles ButtonWrite.Click
         Try
-            Dim filePath As String = "sample.txt"
-
-            Using writer As New StreamWriter(filePath, True) 'true means append mode
+            Using writer As New StreamWriter(filepath, True) 'true means append mode
                 'writer.WriteLine("Hello, this is a test file!")
                 writer.WriteLine("palitan bago.")
                 writer.WriteLine(" ")
@@ -19,10 +18,25 @@ Public Class Form1
     End Sub
 
     Private Sub ButtonRead_Click(sender As Object, e As EventArgs) Handles ButtonRead.Click
-        Dim filepath As String = "sample.txt"
+
         Using reader As New StreamReader(filepath)
             Dim content As String = reader.ReadToEnd()
             MessageBox.Show(content, "File Content", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Using
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonReadPerLine.Click
+        ListBox.Items.Clear()
+        Using reader As New StreamReader(filepath)
+            Dim line As String = reader.ReadLine()
+
+            While line IsNot Nothing
+                ListBox.Items.Add(line)
+                line = reader.ReadLine()
+
+            End While
+
+
         End Using
     End Sub
 End Class
